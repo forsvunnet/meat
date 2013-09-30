c61db0e165945ddfd9132277289d8ed74406f627
Eivin Landa <landa@drivdigital.no>
2013-09-30 13:53:25 +0200

    Added thumbnails to pages and removed blog microdata

--- page-custom.php
+++ page-custom.php
@@ -14,42 +14,44 @@ Template Name: Custom Page Example
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<div id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?>>
 
 								<header class="article-header">
-
-									<h1 class="page-title"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) ), bones_get_the_author_posts_link() );
-									?></p>
-
+									<?php 
+										// A note about thumbnails:
+										// Responsive images is really troublesome as long as there is no set standard
+										// The best solution so far seems to be this:
+									  // http://adaptive-images.com/
+										the_post_thumbnail('full');
+									?>
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
 								</header> <!-- end article header -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
+								<section class="entry-content clearfix">
 									<?php the_content(); ?>
 								</section> <!-- end article section -->
 
 								<footer class="article-footer">
-									<p class="clearfix"><?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+									<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
 
 								</footer> <!-- end article footer -->
 
 								<?php comments_template(); ?>
 
-							</article> <!-- end article -->
+							</div> <!-- end article -->
 
 							<?php endwhile; else : ?>
 
 									<article id="post-not-found" class="hentry clearfix">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										<header class="article-header">
+											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
 										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										<section class="entry-content">
+											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
+												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
 										</footer>
 									</article>
 
--- page.php
+++ page.php
@@ -8,21 +8,23 @@
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<div id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?>>
 
 								<header class="article-header">
-
+									<?php 
+										// A note about thumbnails:
+										// Responsive images is really troublesome as long as there is no set standard
+										// The best solution so far seems to be this:
+									  // http://adaptive-images.com/
+										the_post_thumbnail('full');
+									?>
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) ), bones_get_the_author_posts_link());
-									?></p>
-
 
 								</header> <!-- end article header -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
+								<section class="entry-content clearfix">
 									<?php the_content(); ?>
-							</section> <!-- end article section -->
+								</section> <!-- end article section -->
 
 								<footer class="article-footer">
 									<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
@@ -31,7 +33,7 @@
 
 								<?php comments_template(); ?>
 
-							</article> <!-- end article -->
+							</div> <!-- end article -->
 
 							<?php endwhile; else : ?>
 
